f53ad5d212f0ba932568338b72973231a92149eb
mariam <mariamemampp00@gmail.com>
2025-07-20 14:39:45 +0300

    add routes for the verification

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,24 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register',[AuthController::class ,'register']);
 Route::post('/login',[AuthController::class ,'login']);
-Route::post('/logout',[AuthController::class , 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function (){
+    //logout endpoint
+    Route::post('/logout',[AuthController::class , 'logout']);
+
+    Route::post('email/verification-notification', function(Request $request){
+        if($request->user()->hasverifiedEmail()){
+            return response()->json(['message' => 'Email already verified'], 200);
+        };
+
+        $request->user()->sendEmailVerificationNotification();
+        return response()->json(['message' => 'Verification email sent'], 200);
+    });
+
+    Route::get('email/verify/{id}/{hash}',function(EmailVerificationRequest $request){
+        $request->fulfill();
+        return response()->json(['message' => 'Email verified successfully'], 200);
+    })->name('verification.verify')->middleware(['signed']);
+});
+
+
